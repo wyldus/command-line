061045ff8a5cb66a5b7706134ae13683ed1e9dc6
dagoberto <berto35572@gmail.com>
2021-06-07 11:42:45 -0600

    implemented simple program to read command line

---
+++ src/main/java/edu/cnm/deepdive/CommandLine.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive;
+
+public class CommandLine {
+
+  public static void  main (String[] args){
+    for (String arg : args) {
+      System.out.println(arg.toUpperCase());
+    }
+  }
+
+
+
+}
